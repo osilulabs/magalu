0a9b2eff62c78448a3dbf967d481deb34fc3b176
Osiel <osiel.oliveira@luizalabs.com>
2022-09-02 15:29:30 -0300

    Criado 2ª pasta e 3 exercicios

---
+++ Estrutura_de_decisao/0001.py
@@ -0,0 +1,7 @@
+'''Faça um Programa que peça dois números e imprima o maior deles.'''
+
+
+numero1 = float(input("Digite o primeiro número"))
+numero2 = float(input("Digite o segundo número"))
+
+print(max(numero1, numero2))
---
+++ Estrutura_de_decisao/0002.py
@@ -0,0 +1,9 @@
+'''Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo.'''
+
+numero1 = float(input("Digite o primeiro número: "))
+
+if numero1 <0:
+    print("O número é negativo")
+
+else:
+    print("O número é positivo")
