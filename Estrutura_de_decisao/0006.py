deb6624e1973d3a30d7c9c4c29aa50786b35e1df
Osiel <osiel.oliveira@luizalabs.com>
2022-09-02 23:44:06 -0300

    finalizado arquivo 0006.py

---
+++ Estrutura_de_decisao/0006.py
@@ -0,0 +1,16 @@
+'''Faça um Programa que leia três números e mostre o maior deles.
+'''
+
+num1 = float(input("Digite o primeiro número"))
+num2 = float(input("Digite o segundo número"))
+num3 = float(input("Digite o terceiro número"))
+
+# print("O maior número é: ", max(num1, num2, num3))
+
+''' if num1 > num2 and num3:
+    print(num1, " é o maior número")
+elif num2 >num1 and num3:
+    print(num2, " é o maior número")
+else:
+    print(num3, " é o maior número")
+'''
