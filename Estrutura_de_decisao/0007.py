e23ef66cc8303d207561d974264b0bf1af18e4be
Osiel <osiel.oliveira@luizalabs.com>
2022-09-02 23:47:34 -0300

    Finalizado arquivo 0007.py

---
+++ Estrutura_de_decisao/0007.py
@@ -0,0 +1,8 @@
+'''Faça um Programa que leia três números e mostre o maior e o menor deles.
+'''
+
+num1 = float(input("Digite o primeiro número"))
+num2 = float(input("Digite o segundo número"))
+num3 = float(input("Digite o terceiro número"))
+
+print("O menor número é: ", min(num1, num2, num3))
