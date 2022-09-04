85a22582a3021fab77364c18c8856943d632736d
Osiel <osiel.oliveira@luizalabs.com>
2022-09-04 01:10:07 -0300

    Iniciando arquivo das equaçoes

---
+++ Estrutura_de_decisao/0016.py
@@ -0,0 +1,16 @@
+'''Faça um programa que calcule as raízes de uma equação do segundo grau, na forma ax2 + bx + c. O programa deverá pedir os valores de a, b e c e fazer as consistências, informando ao usuário nas seguintes situações:
+Se o usuário informar o valor de A igual a zero, a equação não é do segundo grau e o programa não deve fazer pedir os demais valores, sendo encerrado;
+Se o delta calculado for negativo, a equação não possui raizes reais. Informe ao usuário e encerre o programa;
+Se o delta calculado for igual a zero a equação possui apenas uma raiz real; informe-a ao usuário;
+Se o delta for positivo, a equação possui duas raiz reais; informe-as ao usuário;'''
+import sys
+
+a = float(input("Insira o valor de A:\n"))
+if a == 0:
+    sys.exit(0)
+b = float(input("Insira o valor de B:\n"))
+c = float(input("Insira o valor de C:\n"))
+
+
+
+
