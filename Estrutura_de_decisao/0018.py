aef31ab40e7932a7315907c1e1ec4f20f38adc12
Osiel <osiel.oliveira@luizalabs.com>
2022-09-04 22:41:48 -0300

    Diminui o tamanho e quantidade de Elif

--- Estrutura_de_decisao/0018.py
+++ Estrutura_de_decisao/0018.py
@@ -11,27 +11,13 @@ mes31 = {1, 3, 5, 7, 8, 10, 12}
 mes30 = {4, 6, 9, 11}
 mes29 = {}
 mes28 = {}
+
 if (ano.endswith("00") and int(ano) % 400 != 0) or (int(ano) % 4 != 0):
     mes28 = {2}
 else:
     mes29 = {2}
 
-if data[2] != '/' or data[5] != '/':
-    print("Data inválida")
-
-elif dia > 31 or dia == 0:
-    print("Data inválida")
-
-elif mes > 12 or mes == 0:
-    print("Data inválida")
-
-elif dia > 30 and mes not in mes31:
-    print("Data inválida")
-
-elif dia > 29 and (mes not in mes31 and mes not in mes30):
-    print("Data inválida")
-
-elif dia > 28 and (mes not in mes29 and mes not in mes31 and mes not in mes30):
+if (data[2] != '/' or data[5] != '/') or (dia > 31 or dia == 0) or (mes > 12 or mes == 0) or (mes > 12 or mes == 0) or (dia > 30 and mes not in mes31) or (dia > 29 and (mes not in mes31 and mes not in mes30)) or (dia > 28 and (mes not in mes29 and mes not in mes31 and mes not in mes30)):
     print("Data inválida")
 
 else:
